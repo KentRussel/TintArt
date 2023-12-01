8a27402ff62d8afcbcd42916b66d46d5624f0ef8
Kent Russel Payumo <kentrusselpayumo@gmail.com>
2023-12-02 00:54:14 +0800

    try

--- components/layout-components/FacebookMsg.jsx
+++ components/layout-components/FacebookMsg.jsx
@@ -1,11 +1,11 @@
 "use client";
-
+import React from 'react';
 import { FacebookProvider, CustomChat } from 'react-facebook';
 
 const FacebookMsg = () => {
     return (
       <FacebookProvider appId="1059396631863694" chatSupport>
-        <CustomChat pageId="113081511298424" minimized={true}/>
+        <CustomChat pageId="113081511298424" minimized={false}/>
       </FacebookProvider>    
     );
   }
