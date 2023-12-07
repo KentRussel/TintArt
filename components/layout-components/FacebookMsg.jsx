4d9778973a7ae25b074b98e1a1de7370a88a0474
Kent Russel Payumo <kentrusselpayumo@gmail.com>
2023-12-08 00:22:33 +0800

    Update FacebookMsg.jsx

--- components/layout-components/FacebookMsg.jsx
+++ components/layout-components/FacebookMsg.jsx
@@ -1,56 +1,34 @@
-import React, { useEffect, useRef } from 'react';
-import { useAppContext } from '../../context/AppContext';
+"use client";
+import React, { Component, useEffect, useRef } from 'react';
 
 const FacebookMsg = () => {
-  const { state } = useAppContext();
-
-  useEffect(() => {
-    if (state.isAuth && (state?.user?.role === 3 || state?.user?.role === 2)) {
-      return; // Do nothing if the user role is 3 or 2
-    }
-
-    // Logic handling for messenger API
-    const messengerRef = useRef(null);
-
-    const initializeFacebookSDK = () => {
-      window.fbAsyncInit = function () {
-        window.FB.init({
-          xfbml: true,
-          version: 'v18.0'
-        });
-      };
-
-      (function (d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));
-    };
-
-    const setupMessengerAttributes = () => {
-      if (messengerRef.current) {
-        messengerRef.current.setAttribute("page_id", "113081511298424");
-        messengerRef.current.setAttribute("attribution", "biz_inbox");
-      }
-    };
-
-    initializeFacebookSDK();
-    setupMessengerAttributes();
-  }, [state.isAuth, state?.user?.role]);
-
+//logic handling for messenger API
+const messengerRef = useRef(null);
+useEffect(()=>{
+  messengerRef.current.setAttribute("page_id", "113081511298424");
+  messengerRef.current.setAttribute("attribution", "biz_inbox");
+  window.fbAsyncInit = function() {
+            FB.init({
+              xfbml  : true,
+              version: 'v18.0'
+            });
+          };
+    
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+},[])
   return (
-    <>
-      {state?.user?.role === 0 && state?.user?.role !== 2 && (
-        <>
-          <div id="fb-root"></div>
-          <div ref={messengerRef} id="fb-customer-chat" className="fb-customerchat">
-          </div>
-        </>
-      )}
-    </>
-  );
-};
+  <>
+      <div id="fb-root"></div>
+      <div ref={(e)=>messengerRef.current = e} id="fb-customer-chat" class="fb-customerchat">
+      </div>  
+  </>
+  )
+  }
 
 export default FacebookMsg;
