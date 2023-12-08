c3a49c2d14445c006470165a04d50faebfbbc714
Kent Russel Payumo <kentrusselpayumo@gmail.com>
2023-12-08 14:31:02 +0800

    Update FacebookMsg.jsx

--- components/layout-components/FacebookMsg.jsx
+++ components/layout-components/FacebookMsg.jsx
@@ -1,10 +1,20 @@
 "use client";
 import React, { Component, useEffect, useRef } from 'react';
+import { useAppContext } from '../../context/AppContext';
+import { useRouter } from 'next/router';
 
 const FacebookMsg = () => {
+  const router = useRouter();
 //logic handling for messenger API
+const path = (router.pathname).split("/");
+for(let i = 0; i <= path.length; i++){
+  if(path[i] == "admin"){
+    return;
+  }
+}
 const messengerRef = useRef(null);
 useEffect(()=>{
+  if(messengerRef.current == null) return;
   messengerRef.current.setAttribute("page_id", "113081511298424");
   messengerRef.current.setAttribute("attribution", "biz_inbox");
   window.fbAsyncInit = function() {
@@ -13,7 +23,6 @@ useEffect(()=>{
               version: 'v18.0'
             });
           };
-    
   (function(d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) return;
@@ -21,12 +30,12 @@ useEffect(()=>{
     js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
     fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'facebook-jssdk'));
-},[])
+},[messengerRef.current])
   return (
   <>
-      <div id="fb-root"></div>
+    <div id="fb-root"></div>
       <div ref={(e)=>messengerRef.current = e} id="fb-customer-chat" class="fb-customerchat">
-      </div>  
+    </div>  
   </>
   )
   }
